aaaba6ca6188698927d053cd8b6e3c25bceccf1b
pedrosoutinho <johnetterna@protonmail.com>
2023-10-02 02:00:04 -0300

    importar bootstrap js localmente

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './globals.css'
 import Footer from './components/footer'
 import Navbar from './components/navbar'
-import './globals.css'
+import ImportBsJS from './components/importBsJS'
 import { Inter } from 'next/font/google'
-import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
                 <Navbar />
                 {children}
                 <Footer />
-                <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" />
+                <ImportBsJS />
             </body>
         </html>
     )
