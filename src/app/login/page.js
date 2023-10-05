4a26ade5c2b2beb49cb9f7ef9863c2f1ad801b16
Isaac <isaacjoseramos@gmail.com>
2023-10-05 18:14:46 -0300

    comentario

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -16,14 +16,14 @@ function Page() {
     // Adiciona uma sombra suave ao redor do formulário
     boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
   };
-  
+
   const labelStyle = {
     // Define o rótulo como um bloco, para que ele ocupe toda a largura disponível
     display: 'block',
     // Adiciona um espaçamento inferior para separar os rótulos dos campos de entrada
     marginBottom: '8px',
   };
-  
+
   const inputStyle = {
     // Define a largura do campo de entrada como 100% da largura do pai
     width: '100%',
@@ -36,8 +36,8 @@ function Page() {
     // Adiciona uma borda ao redor do campo de entrada
     border: '1px solid #ccc',
   };
-  
-  const buttonStyle = {  //indica uma propriedade dinamica
+
+  const buttonStyle = {
     // Define a cor de fundo do botão
     backgroundColor: '#007BFF',
     // Define a cor do texto do botão como branco
@@ -52,45 +52,47 @@ function Page() {
     cursor: 'pointer',
   };
 
-    return (
-        <main>
-            <h1>Acesse a sua conta</h1>
-            <div>
-                <form style={formStyle}>
-                    <div className="form-group">
-                        <label style={labelStyle} htmlFor="exampleInputEmail1">
-                            Endereço de email
-                        </label>
-                        <input
-                            style={inputStyle}
-                            type="email"
-                            id="exampleInputEmail1"
-                            aria-describedby="emailHelp"
-                            placeholder="Seu email"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label style={labelStyle} htmlFor="exampleInputPassword1">
-                            Senha
-                        </label>
-                        <input
-                            style={inputStyle}
-                            type="password"
-                            id="exampleInputPassword1"
-                            placeholder="Senha"
-                        />
-                    </div>
-                    <div className="form-group form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" htmlFor="exampleCheck1">
-                            Manter minha conta conectada
-                        </label>
-                    </div>
-                    <button type="submit" style={buttonStyle}>
-                        Entrar
-                    </button>
-                </form>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <h1>Acesse a sua conta</h1>
+      <div>
+        <form style={formStyle}>
+          <div className="form-group">
+            <label style={labelStyle} htmlFor="exampleInputEmail1">
+              Endereço de email
+            </label>
+            <input
+              style={inputStyle}
+              type="email"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Seu email"
+            />
+          </div>
+          <div className="form-group">
+            <label style={labelStyle} htmlFor="exampleInputPassword1">
+              Senha
+            </label>
+            <input
+              style={inputStyle}
+              type="password"
+              id="exampleInputPassword1"
+              placeholder="Senha"
+            />
+          </div>
+          <div className="form-group form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Manter minha conta conectada
+            </label>
+          </div>
+          <button type="submit" style={buttonStyle}>
+            Entrar
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 }
+
+export default Page; // Exporte o componente como o valor padrão
